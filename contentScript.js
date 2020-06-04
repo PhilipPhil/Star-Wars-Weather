0bd31653834fd825d5cb62ca14171f694de23391
PhilipPhil <philipgarabandic@gmail.com>
2020-06-03 23:18:49 -0400

    no message

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,30 @@
-let file = 'img/hoth/1.jpg'
-let url = chrome.extension.getURL(file)
-// document.body.style.background = "#f3f3f3 url('" + url + "') no-repeat right auto";
-document.body.style.background = "url('" + url + "')"
-document.body.style.fontWeight = ['bold']
+function setBackgroundImage(temp) {
+    let file
+    if(temp > 287) {
+        file = 'img/hoth/1.jpg'
+    } else {
+        file = 'img/coruscant/1.jpg'
+    }
+    let url = chrome.extension.getURL(file)
+    document.body.style.background = "url('" + url + "')"
+    document.body.style.fontWeight = ['bold']
+
+}
+
+
+function createBackGround(data) {
+    temp = data["main"]["temp"]
+    setBackgroundImage(temp)
+}
+
+function showMap(position) {
+    lat = position.coords.latitude
+    lon = position.coords.longitude
+    apikey = '3e7cd6048bd8114abedeee14fcc11575'
+    fetch('https://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lon+'&appid='+apikey)
+        .then(response => response.json())
+        .then(data => createBackGround(data));
+}
+
+navigator.geolocation.getCurrentPosition(showMap);
+
