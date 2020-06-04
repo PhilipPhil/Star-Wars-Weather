454fb06520f54b26e83eb319c1584609f6270ddb
PhilipPhil <philipgarabandic@gmail.com>
2020-06-03 22:08:59 -0400

    no message

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,5 @@
-alert('working')
+let file = 'img/hoth/1.jpg'
+let url = chrome.extension.getURL(file)
+// document.body.style.background = "#f3f3f3 url('" + url + "') no-repeat right auto";
+document.body.style.background = "url('" + url + "')"
+document.body.style.fontWeight = ['bold']
