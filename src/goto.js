ef80e8b27b5e343dd87692c531311b6264b5c22d
PhilipPhil <philipgarabandic@gmail.com>
2020-06-18 13:56:57 -0400

    no message

--- src/goto.js
+++ src/goto.js
@@ -1,11 +1,7 @@
 document.getElementById('goToApps').addEventListener('click', function(e) {
-    chrome.tabs.create({"url":"chrome://apps"})
+    chrome.tabs.update({url:"chrome://apps"})
 
 })
-
 document.getElementById('goToDefault').addEventListener('click', function(e) {
-    chrome.tabs.query ({ active: true, currentWindow: true }, tabs => {
-        if (tabs[0]) { chrome.tabs.reload (tabs[0].id) }
-        chrome.runtime.reload ()
-      })
+    chrome.tabs.update({url:"chrome-search://local-ntp/local-ntp.html"})
 })
